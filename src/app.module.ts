6adb7d7cb52aca998ea5831924928af79a94121d
OlekZima <zimenkoalex34@gmail.com>
2024-01-06 22:37:56 +0100

    Answer class/module/service/controller created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuestionModule } from './question/question.module';
+import { AnswerModule } from './answer/answer.module';
 
 @Module({
-  imports: [],
+  imports: [QuestionModule, AnswerModule],
   controllers: [AppController],
   providers: [AppService],
 })
