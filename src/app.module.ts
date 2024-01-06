58c997779b4a581348b9b1afc7945ff24336cd37
OlekZima <zimenkoalex34@gmail.com>
2024-01-06 22:45:50 +0100

    Import fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuestionModule } from './question/question.module';
-import { AnswerModule } from './question/answer/answer.module';
+import { AnswerModule } from './quiz/question/answer/answer.module';
+import { QuestionModule } from './quiz/question/question.module';
 
 @Module({
   imports: [QuestionModule, AnswerModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
