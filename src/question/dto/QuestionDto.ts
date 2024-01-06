e7e0664a742627ef73a2259f6d8b10c5c996a777
OlekZima <zimenkoalex34@gmail.com>
2024-01-06 22:37:10 +0100

    Question class created

---
+++ src/question/dto/QuestionDto.ts
@@ -0,0 +1,18 @@
+import { Answer } from "src/answer/dto/AnswerDto";
+
+export class Question {
+  id: number;
+  question: string;
+  possible_answers: Answer[];
+  correct_answers: Answer[];
+
+  constructor(id: number, question: string, possible_answers: Answer[], correct_answers: Answer[]) {
+    if (!possible_answers.some(answer => correct_answers.includes(answer))) {
+      throw new Error("Correct answer must be one of the possible answers");
+    }
+    this.id = id;
+    this.question = question;
+    this.possible_answers = possible_answers;
+    this.correct_answers = correct_answers;
+  }
+}
