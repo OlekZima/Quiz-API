db2337d8255198ab45e81991b61bf9e6175fa871
OlekZima <zimenkoalex34@gmail.com>
2024-01-06 22:38:07 +0100

    SimpleQuestion class created

---
+++ src/question/simple-question/dto/SimpleQuestionDto.ts
@@ -0,0 +1,13 @@
+import { Answer } from "src/answer/dto/AnswerDto";
+import { Question } from "src/question/dto/QuestionDto";
+
+export class SimpleQuestion extends Question {
+  constructor(id: number, question: string, possible_answers: Answer[], correct_answer: Answer[]) {
+
+    if (correct_answer.length > 1) {
+      throw new Error("Simple question can only have one correct answer");
+    }
+
+    super(id, question, possible_answers, correct_answer);
+  }
+}
